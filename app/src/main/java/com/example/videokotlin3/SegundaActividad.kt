305729043d607d0cb2857bfa095784a7ce7c0775
jlianacastillo <lmov261220112@merida.tecnm.mx>
2023-03-22 15:01:32 -0600

    Initial commit

---
+++ app/src/main/java/com/example/videokotlin3/SegundaActividad.kt
@@ -0,0 +1,11 @@
+package com.example.videokotlin3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SegundaActividad : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda_actividad)
+    }
+}
